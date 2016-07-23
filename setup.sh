a8b1c421b5912d085cf0ab73de2dd9945b785627
Bennett Marsh <bmarsh9311@gmail.com>
2016-07-22 17:33:42 -0700

    reorganizing

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 
-echo "Setting PYTHONPATH environment variable"
-export PYTHONPATH=$PWD:$PYTHONPATH
+echo "Setting PYTHONPATH environment variable..."
+export PYTHONPATH=${PWD}/src:$PYTHONPATH
 
-python setup.py
+echo "Checking for required modules..."
+
+echo -e "try:\n    import numpy\n    print \"-Successfully loaded numpy\"\n\
+except:\n    print\"ERROR: could not import numpy!\"\n    exit(1)" | python
+
+echo -e "try:\n    import matplotlib.pyplot\n    print \"-Successfully loaded matplotlib\"\n\
+except:\n    print\"WARNING: could not import matplotlib!\"\n    exit(1)" | python
+
+echo -e "try:\n    import ROOT\n    print \"-Successfully loaded ROOT\"\n\
+except:\n    print\"WARNING: could not import pyROOT!\"\n    exit(1)" | python
+
+
